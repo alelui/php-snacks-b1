5cc7c179f8014382e7cecd889fbb8be061ef4906
Alessandro <luisialessandro95@gmail.com>
2022-01-18 19:48:15 +0100

    snakToe

--- Snack-2/index.php
+++ Snack-2/index.php
@@ -1,18 +1,17 @@
 <!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
 <?php
-    
-?>
+    $name = $_GET['name']; 
+    $mail = $_GET['mail']; 
+    $age = $_GET['age'];
+    // var_dump(strlen($name));
+    $paraOne = '@';
+    $paraTow = '.';
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+if(strlen($name) > 3 && is_numeric($age) && strpos($mail, $paraOne) && strpos($mail, $paraTow)){
+    echo 'Accesso riuscito';
+}else{
+    echo 'Accesso negato';
+}
+//?name=gigi&age=8&mail=gigi@supertritone.com
+?>
